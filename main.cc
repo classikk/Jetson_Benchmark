fc2517d9b1755ee89115c1a6455450a101c4ace6
Lassi <lassi.kokkonen@aalto.fi>
2025-11-19 16:52:30 +0200

    timer

--- main.cc
+++ main.cc
@@ -1,7 +1,14 @@
 #include <iostream>
+#include <chrono>
 using namespace std;
 
-int main() {
-    return 0;
+static inline void timer(std::chrono::time_point<std::chrono::steady_clock> &start){
+    auto time = std::chrono::steady_clock::now();
+    cout << "[" << (start-time).count() << "s]"<< endl;
+}
 
+int main() {
+    std::chrono::time_point<std::chrono::steady_clock> start_time  = std::chrono::steady_clock::now();
+    timer(start_time);
+    return 0;
 }
