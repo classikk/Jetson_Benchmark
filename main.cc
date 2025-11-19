2a1ab1846e7e9dcf00bab98e0852630773a1d5e6
classikk <lassi04kokkonen@gmail.com>
2025-11-19 17:15:42 +0200

    timer fix

--- main.cc
+++ main.cc
@@ -1,14 +1,16 @@
 #include <iostream>
 #include <chrono>
+#include <thread>
 using namespace std;
 
 static inline void timer(std::chrono::time_point<std::chrono::steady_clock> &start){
     auto time = std::chrono::steady_clock::now();
-    cout << "[" << (start-time).count() << "s]"<< endl;
+    cout << "[" << (double)(time-start).count()/1000000000 << "s]"<< endl;
 }
 
 int main() {
     std::chrono::time_point<std::chrono::steady_clock> start_time  = std::chrono::steady_clock::now();
+    
     timer(start_time);
     return 0;
 }
