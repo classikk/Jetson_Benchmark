43e71b9c20e5d2d4663bf9a6089840dd560e37fc
classikk <lassi04kokkonen@gmail.com>
2026-01-26 20:07:48 +0200

    IMX219 setup

--- JetsonSetup/install_vscode.sh
+++ JetsonSetup/install_vscode.sh
@@ -2,15 +2,16 @@
 
 # URL for VS Code ARM64 .deb
 URL="https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-arm64"
-
-# Output filename
 FILE="vscode-arm64.deb"
+
+# Disable Microsoft repo auto‑add
+sudo mkdir -p /etc/vscode
+echo "false" | sudo tee /etc/vscode/disable-repo > /dev/null
 
 echo "Downloading Visual Studio Code..."
 curl -L "$URL" -o "$FILE"
 
-echo "Installing Visual Studio Code..."
+echo "Installing Visual Studio Code without adding Microsoft repo..."
 sudo apt install ./"$FILE"
 
 echo "Done."
-
